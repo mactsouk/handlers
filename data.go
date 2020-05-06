f1d18b0ff448ab1dbe3b7d49f6fbf7b26c9ac647
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-06 18:55:32 +0300

    Minor correction

--- data.go
+++ data.go
@@ -1,10 +1,12 @@
 package handlers
 
+import "time"
+
 type User struct {
-	ID        int    `json:"id"`
-	Username  string `json:"user"`
-	Password  string `json:"password"`
-	LastLogin time   `json:"lastlogin"`
-	Admin     bool   `json:"admin"`
-	Active    bool   `json:"active"`
+	ID        int       `json:"id"`
+	Username  string    `json:"user"`
+	Password  string    `json:"password"`
+	LastLogin time.Time `json:"lastlogin"`
+	Admin     bool      `json:"admin"`
+	Active    bool      `json:"active"`
 }
