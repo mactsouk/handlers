d714d3f82a21769c78d1d042a80e1cddecf50d14
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-07 13:45:15 +0300

    Function + Method definitions

--- data.go
+++ data.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -21,18 +23,44 @@ type Input struct {
 	Admin    bool   `json:"admin"`
 }
 
-func AddUser() {
+// AddUser is for adding a new user to the database
+func AddUser(u User) bool {
 
+	return true
 }
 
-func DeleteUser() {
+// DeleteUser is for deleting a user defined by ID
+func DeleteUser(int ID) bool {
 
+	return true
 }
 
-func ReturnAllUsers() {
+// ReturnAllUsers is for returning all users from database
+func ReturnAllUsers() []User {
 
+	return nil
 }
 
-func FindUser() {
+// FindUserID is for returning a user record defined by ID
+func FindUserID(int ID) User {
 
+	return nil
+}
+
+// FindUserUsername is for returning a user record defined by username
+func FindUserUsername(string username) User {
+
+	return nil
+}
+
+// FromJSON decodes a serialized JSON record
+func (p *User) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
+// ToJSON encodes a JSON record
+func (p *User) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
 }
