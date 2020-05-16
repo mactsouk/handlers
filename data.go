7332c0fb8eeeeabce775637c307e703c4e1809da
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-16 18:26:42 +0300

    Implement FindUserUsername(username string) User

--- data.go
+++ data.go
@@ -202,6 +202,35 @@ func FindUserID(ID int) User {
 
 // FindUserUsername is for returning a user record defined by username
 func FindUserUsername(username string) User {
+	log.Println("Get User Data from SQLite3:", ID)
+	db, err := sql.Open("sqlite3", SQLFILE)
+	if err != nil {
+		log.Println(err)
+		return User{}
+	}
+	defer db.Close()
 
-	return User{}
+	rows, err := db.Query("SELECT * FROM users where Username = $1 \n", username)
+	if err != nil {
+		log.Println("FindUserUsername Query:", err)
+		return User{}
+	}
+	defer rows.Close()
+
+	u := User{}
+	var c1 int
+	var c2, c3 string
+	var c4 int64
+	var c5, c6 bool
+
+	for rows.Next() {
+		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		if err != nil {
+			log.Println(err)
+			return User{}
+		}
+		u = User{c1, c2, c3, c4, c5, c6}
+		log.Println("Found user:", u)
+	}
+	return u
 }
