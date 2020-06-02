d345e9489685306b0351d704f3cbe82d3ccbe9b4
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-02 19:07:07 +0300

    Empty handler functions for send and upload file

--- v2.go
+++ v2.go
@@ -1,7 +1,17 @@
 package handlers
 
+import "net/http"
+
 type V2Input struct {
 	Username  string `json:"username"`
 	Upassword string `json:"password"`
 	U         User   `json:"load"`
+}
+
+func uploadFile(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func sendFile(rw http.ResponseWriter, r *http.Request) {
+
 }
