3157cd6f2e3b1c10759555b115a9a8eed7a7f9b5
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-06-03 18:27:16 +0300

    Upload functionality + Create Image Directory

--- v2.go
+++ v2.go
@@ -1,6 +1,15 @@
 package handlers
 
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+var IMAGESPATH = "/tmp/files"
 
 type V2Input struct {
 	Username  string `json:"username"`
@@ -9,9 +18,51 @@ type V2Input struct {
 }
 
 func UploadFile(rw http.ResponseWriter, r *http.Request) {
-
+	filename, ok := mux.Vars(r)["filename"]
+	if !ok {
+		log.Println("filename value not set!")
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+	log.Println(filename)
+	saveFile(IMAGESPATH+"/"+filename, rw, r)
 }
 
-func SendFile(rw http.ResponseWriter, r *http.Request) {
+func saveFile(path string, rw http.ResponseWriter, r *http.Request) {
+	log.Println("Saving to", path)
+	err := saveToFile(path, r.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
 
+func CreateImageDirectory(d string) error {
+	_, err := os.Stat(IMAGESPATH)
+	if os.IsNotExist(err) {
+		log.Println("Creating:", IMAGESPATH)
+		err = os.MkdirAll(IMAGESPATH, 0755)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	} else if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	fileInfo, err := os.Stat(IMAGESPATH)
+	mode := fileInfo.Mode()
+	if !mode.IsDir() {
+		msg := IMAGESPATH + " is not a directory!"
+		return errors.New(msg)
+	}
+	return nil
+}
+
+func MiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Serving %s from %s", r.RequestURI, r.Host)
+		next.ServeHTTP(w, r)
+	})
 }
